20c0ff90c13dab01e690ad9ae74172187b42dda7
Кирилл <kkirillcore@gmail.com>
2018-09-16 18:28:58 +0300

    пока все дома (исправления)

--- js/map.js
+++ js/map.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var getRandomInRange = function(min, max) {
+var getRandomInRange = function (min, max) {
   var rand = min - 0.5 + Math.random() * (max - min + 1);
   rand = Math.round(rand);
   return rand;
 };
 
-var getRandomValue = function(arr) {
+var getRandomValue = function (arr) {
   var min = 0;
   var max = arr.length - 1;
   var value = getRandomInRange(min, max);
   return arr[value];
 };
 
-var getfeatures = function(array) {
-  var arr2 = arr.slice(Math.Floor(Math.Random() * array.length));
+var getfeatures = function (array) {
+  var arr = arr.slice(Math.floor(Math.random() * array.length));
 };
 
 var titleValues = [
@@ -49,31 +49,32 @@ var photosValues = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 
-var getAdvertisement = function(index) {
+var getAdvertisement = function (index) {
   var author = {
     avatar: 'img/avatars/user0' + index + '.png'
-  },
+  };
 
   var location = {
     x: 100,
     y: getRandomInRange(130, 630)
-  },
+  };
 
   var offer = {
-    title: getRandomValue(title),
+    title: getRandomValue(titleValues),
     address: location.x + ', ' + location.y,
     price: getRandomInRange(1000, 1000000),
-    type: getRandomValue(type),
+    type: getRandomValue(typeValues),
     rooms: getRandomInRange(1, 5),
-    guests: Math.Floor(Math.Random() * 10),
+    guests: Math.floor(Math.random() * 10),
     checkin: getRandomValue(checkinValues),
     checkout: getRandomValue(checkoutValues),
-    features: getfeatures(features),
+    features: getfeatures(featuresValues),
     description: ' ',
-    photos: photosValues.sort(function() {
+    photos: photosValues.sort(function () {
       return Math.random() - 0.5;
-     })
-  }
+    })
+  };
+
   return {
     author: author,
     offer: offer,
@@ -81,15 +82,15 @@ var getAdvertisement = function(index) {
   };
 };
 
-var getAdvertisements = function() {
-  array = [];
+var getAdvertisements = function () {
+  var array = [];
   for (var i = 0; i < 8; i++) {
     array[i] = getAdvertisement(i);
   }
   return array;
 };
 
-var advertisements = getAdvertisements ();
+var advertisements = getAdvertisements();
 
 var price = document.querySelector('.map');
 price.classList.remove('.map--faded');
@@ -100,16 +101,11 @@ var template = document
   .querySelector('.map__pin')
   .content.querySelector('button');
 
-for (var i = 0; i < 8; i++) {
+for (var i = 0; i < advertisements.length; i++) {
   var element = template.cloneNode(true);
-  element.advertisements[i];
 
-  element.style =
-    'left: ' +
-    advertisements[i].location.x +
-    ' px; top: ' +
-    advertisements[i].location.y +
-    ' px;';
+  element.style.left = advertisements[i].location.x + 'px';
+  element.style.top = advertisements[i].location.y + 'px';
   element.src = advertisements[i].author.avatar;
   element.alt = advertisements[i].offer.title;
   element.children[0].textContent = i;
