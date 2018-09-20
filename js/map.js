f2342301506d70675f7336504b81b0db8ef3cb2f
Кирилл <kkirillcore@gmail.com>
2018-09-20 22:56:15 +0300

    пока все дома

--- js/map.js
+++ js/map.js
@@ -22,7 +22,7 @@ var getPhotos = function (array) {
   array.sort(function () {
     return Math.random() - 0.5;
   });
-  return getPhotos;
+  return array;
 };
 
 var titleValues = [
@@ -120,42 +120,51 @@ var fragment = document.createDocumentFragment();
 
 newPins.appendChild(fragment);
 
-template = document.querySelector('#card').content;
-var listing = template.querySelector('.map__card').cloneNode(true);
-listing.classList.add('card');
+var templateListing = document.querySelector('#card').content;
 
-advertisements.offer.type.flat = 'Квартира';
-advertisements.offer.type.palace = 'Дворец';
-advertisements.offer.type.house = 'Дом';
-advertisements.offer.type.bungalo = 'Бунагло';
+var listing = templateListing.cloneNode(true);
 
-listing.querySelector('.popup__title').textContent =
-  advertisements[0].offer.title;
+var advertisement = advertisements[0];
+var offerTypes = {
+  flat: 'Квартира',
+  palace: 'Дворец',
+  house: 'Дом',
+  bungalo: 'Бунагло'
+};
+
+listing.querySelector('.popup__title').textContent = advertisement.offer.title;
 listing.querySelector('.popup__text--address').textContent =
-  advertisements[0].offer.address;
+  advertisement.offer.address;
 listing.querySelector('.popup__text--price').textContent =
-  advertisements[0].offer.price + '₽/ночь';
+  advertisement.offer.price + '₽/ночь';
 listing.querySelector('.popup__type').textContent =
-  advertisements[0].offer.type;
+  offerTypes[advertisements[0].offer.type];
 listing.querySelector('.popup__text--capacity').textContent =
-  advertisements[0].offer.rooms +
+  advertisement.offer.rooms +
   ' комнаты для ' +
-  advertisements[0].offer.guests +
+  advertisement.offer.guests +
   ' гостей';
 listing.querySelector('.popup__text--time').textContent =
   'Заезд после ' +
-  advertisements[0].offer.checkin +
+  advertisement.offer.checkin +
   ' , выезд до ' +
-  advertisements[0].offer.checkout;
+  advertisement.offer.checkout;
 listing.querySelector('.popup__features').textContent =
-  advertisements[0].offer.features;
+  advertisement.offer.features;
 listing.querySelector('.popup__description').textContent =
-  advertisements[0].offer.description;
+  advertisement.offer.description;
 
-var listingPhoto = listing.querySelector('.popup__photos').content;
-listingPhoto.src = advertisements[0].offer.photos;
+var listingPhoto = listing.querySelector('.popup__photos');
+var popupPhoto = listingPhoto.querySelector('img');
+popupPhoto.src = advertisement.offer.photos[0];
+
+for (i = 1; i < photosValues.length; i++) {
+  popupPhoto = listingPhoto.querySelector('img').cloneNode(true);
+  popupPhoto.src = advertisement.offer.photos[i];
+  listingPhoto.appendChild(popupPhoto);
+}
 
 var avatar = listing.querySelector('.popup__avatar');
-avatar.src = advertisements[0].offer.author.avatar;
+avatar.src = advertisement.author.avatar;
 
-map.insertBefore(listing, document.map.map__filters - container);
+map.insertBefore(listing, map.querySelector('.map__filters-container'));
